9409d6000eede8f11e46229fc74f9ab4d8610522
bill <bill.mill@gmail.com>
2012-03-16 10:54:55 -0400

    merging Kenny's code

--- src/t_iset.c
+++ src/t_iset.c
@@ -39,21 +39,19 @@ avlNode *avlCreateNode(double lscore, double rscore, robj *obj) {
 	return an;
 }
 
-avlFreeNode(avlNode *node) {
-	decrRefCount(node->obj);
-	if (!node->left) {
+void avlFreeNode(avlNode *node) {
+	if (node->obj)
+		decrRefCount(node->obj);
+	if (!node->left)
 		avlFreeNode(node->left);
-	}	
-	if (!node->right) {
+	if (!node->right)
 		avlFreeNode(node->right);
-	}
 	zfree(node);
 }
 
-avlFree(avl *tree) {
-	if (tree->root != NULL) {
+void avlFree(avl *tree) {
+	if (tree->root != NULL)
 		avlFreeNode(tree->root);
-	}
 	zfree(tree);
 }
 
@@ -61,30 +59,139 @@ avlFree(avl *tree) {
 int avlNodeCmp(avlNode *a, avlNode *b) {
 	if (a->leftScore < b->leftScore)
 		return -1;
-	else
+	else if (a->leftScore > b->leftScore)
 		return 1;
+	else {
+		if (a->rightScore > b->rightScore)
+			return -1;
+		else if (a->rightScore < b->rightScore)
+			return 1;
+		else
+			return 0;
+	}
+}
+
+void avlLeftRotation(avlNode *locNode) {
+	avlNode *newRoot = locNode->right;
+	locNode->right = newRoot->left;
+	newRoot->left = locNode;
+	if (locNode->right)
+		locNode->right->parent = locNode;
+	newRoot->parent = locNode->parent;
+	locNode->parent = newRoot;
+	if (newRoot->parent) {
+		if (avlNodeCmp(newRoot->parent,newRoot) > -1)
+			newRoot->parent->left = newRoot;
+		else
+			newRoot->parent->right = newRoot;
+	}
+}
+
+void avlRightRotation(avlNode *locNode) {
+	avlNode *newRoot = locNode->left;
+	locNode->left = newRoot->right;
+	newRoot->right = locNode;
+	if (locNode->left)
+		locNode->right->parent = locNode;
+	newRoot->parent = locNode->parent;
+	locNode->parent = newRoot;
+	if (newRoot->parent) {
+		if(avlNodeCmp(newRoot->parent,newRoot) > -1)
+			newRoot->parent->left = newRoot;
+		else
+			newRoot->parent->right = newRoot;
+	}
+}
+
+void avlResetBalance(avlNode *locNode) {
+	switch(locNode->balance) {
+		case -1:
+		locNode->left->balance = 0;
+		locNode->right->balance = 1;
+		break;
+		case 0:
+		locNode->left->balance = 0;
+		locNode->right->balance = 0;
+		break;
+		case 1:
+		locNode->left->balance = -1;
+		locNode->right->balance = 0;
+		break;
+	}
+	locNode->balance = 0;
 }
 
 int avlInsertNode(avlNode *locNode, avlNode *insertNode) {
 	/* Insert in the left node */
-	if (avlNodeCmp(locNode,insertNode)) {
+	if (avlNodeCmp(locNode,insertNode) > -1) {
 		if (!locNode->left) {
 			locNode->left = insertNode;
+			insertNode->parent = locNode;
 			locNode->balance = locNode->balance - 1;
-			if (locNode->balance) return 0;
-			return 1;
+			return locNode->balance ? 0 : 1;
+		}
+		else {
+			// Left node is occupied, insert it into the subtree
+			if (avlInsertNode(locNode->left,insertNode)) {
+				locNode->balance = locNode->balance - 1;
+				if (locNode->balance == 0)
+					return 0;
+				else if (locNode->balance == -1)
+					return 1;
+					
+				// Tree is unbalanced at this point
+				if (locNode->left->balance < 0) {
+					// Left-Left, single right rotation needed
+					avlRightRotation(locNode);
+					locNode->right->balance = 0;
+					locNode->parent->balance = 0;
+				}
+				else {
+					// Left-Right, left rotation then right rotation needed
+					avlLeftRotation(locNode->left);
+					avlRightRotation(locNode);
+					avlResetBalance(locNode->parent);
+				}
+			}
+			return 0;
 		}
 	}
 	/* Insert in the right node */
 	else {
 		if (!locNode->right) {
 			locNode->right = insertNode;
+			insertNode->parent = locNode;
 			locNode->balance = locNode->balance + 1;
-			if (locNode->balance) return 0;
-			return 1;
+			return locNode->balance ? 0 : 1;
+		}
+		else {
+			// Right node is occupied, insert it into the subtree
+			if (avlInsertNode(locNode->right,insertNode)) {
+				locNode->balance = locNode->balance - 1;
+				if (locNode->balance == 0)
+					return 0;
+				else if (locNode->balance == -1)
+					return 1;
+					
+				// Tree is unbalanced at this point
+				if (locNode->right->balance > 0) {
+					// Right-Right, single left rotation needed
+					avlLeftRotation(locNode);
+					locNode->left->balance = 0;
+					locNode->parent->balance = 0;
+				}
+				else {
+					// Right-Left, right rotation then left rotation needed
+					avlRightRotation(locNode->right);
+					avlLeftRotation(locNode);
+					avlResetBalance(locNode->parent);
+				}
+			}
+			return 0;
 		}
 	}
 }
+
 
 avlNode *avlInsert(avl *tree, double lscore, double rscore, robj *obj) {
 	avlNode *an = avlCreateNode(lscore, rscore, obj);
@@ -95,6 +202,8 @@ avlNode *avlInsert(avl *tree, double lscore, double rscore, robj *obj) {
 		avlInsertNode(tree->root, an);
 	}
 	
+	tree->size = tree->size + 1;
+	
 	return an;
 }
 
@@ -103,12 +212,12 @@ avlNode *avlInsert(avl *tree, double lscore, double rscore, robj *obj) {
  *----------------------------------------------------------------------------*/
 
 /* This generic command implements both ZADD and ZINCRBY. */
-void iaddGenericCommand(redisClient *c, int incr) {
+void iaddCommand(redisClient *c) {
     robj *key = c->argv[1];
     robj *iobj;
     robj *curobj;
     double min = 0, max = 0;
-    double score = 0, *mins, *maxes;
+    double *mins, *maxes;
     int j, elements = (c->argc-2)/2;
     int added = 0;
 
@@ -169,8 +278,4 @@ void iaddGenericCommand(redisClient *c, int incr) {
     zfree(mins);
     zfree(maxes);
     addReplyLongLong(c,added);
-}
-
-void iaddCommand(redisClient *c) {
-    iaddGenericCommand(c,0);
 }
