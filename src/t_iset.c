8a1155b07bcdb7c149cc1de49046ed521f47560a
bill <bill.mill@gmail.com>
2012-03-16 01:02:13 -0400

    starting to hack away at an IADD implementation

--- src/t_iset.c
+++ src/t_iset.c
@@ -97,3 +97,162 @@ avlNode *avlInsert(avl *tree, double lscore, double rscore, robj *obj) {
 	
 	return an;
 }
+
+/*-----------------------------------------------------------------------------
+ * Interval set commands 
+ *----------------------------------------------------------------------------*/
+
+/* This generic command implements both ZADD and ZINCRBY. */
+void iaddGenericCommand(redisClient *c, int incr) {
+    static char *nanerr = "resulting score is not a number (NaN)";
+    robj *key = c->argv[1];
+    robj *ele;
+    robj *iobj;
+    robj *curobj;
+    double min = 0, max = 0;
+    double score = 0, *mins, *maxes, curscore = 0.0;
+    int j, elements = (c->argc-2)/2;
+    int added = 0;
+
+    /* 5, 8, 11... arguments */
+    if ((c->argc - 2) % 3) {
+        addReply(c,shared.syntaxerr);
+        return;
+    }
+
+    /* Start parsing all the scores, we need to emit any syntax error
+     * before executing additions to the sorted set, as the command should
+     * either execute fully or nothing at all. */
+    mins = zmalloc(sizeof(double)*elements);
+    maxes = zmalloc(sizeof(double)*elements);
+    for (j = 0; j < elements; j++) {
+        /* mins are 2, 5, 8... */
+        if (getDoubleFromObjectOrReply(c,c->argv[2+j*3],&mins[j],NULL)
+            != REDIS_OK)
+        {
+            zfree(mins);
+            zfree(maxes);
+            return;
+        }
+        /* maxes are 3, 6, 9... */
+        if (getDoubleFromObjectOrReply(c,c->argv[3+j*3],&maxes[j],NULL)
+            != REDIS_OK)
+        {
+            zfree(mins);
+            zfree(maxes);
+            return;
+        }
+    }
+
+    /* Lookup the key and create the sorted set if does not exist. */
+    iobj = lookupKeyWrite(c->db,key);
+    if (iobj == NULL) {
+        iobj = avlCreate();
+        dbAdd(c->db,key,iobj);
+    } else {
+        if (iobj->type != REDIS_ISET) {
+            addReply(c,shared.wrongtypeerr);
+            zfree(scores);
+            return;
+        }
+    }
+
+    for (j = 0; j < elements; j++) {
+        score = scores[j];
+
+        if (zobj->encoding == REDIS_ENCODING_ZIPLIST) {
+            unsigned char *eptr;
+
+            /* Prefer non-encoded element when dealing with ziplists. */
+            ele = c->argv[3+j*2];
+            if ((eptr = zzlFind(zobj->ptr,ele,&curscore)) != NULL) {
+                if (incr) {
+                    score += curscore;
+                    if (isnan(score)) {
+                        addReplyError(c,nanerr);
+                        /* Don't need to check if the sorted set is empty
+                         * because we know it has at least one element. */
+                        zfree(scores);
+                        return;
+                    }
+                }
+
+                /* Remove and re-insert when score changed. */
+                if (score != curscore) {
+                    zobj->ptr = zzlDelete(zobj->ptr,eptr);
+                    zobj->ptr = zzlInsert(zobj->ptr,ele,score);
+
+                    signalModifiedKey(c->db,key);
+                    server.dirty++;
+                }
+            } else {
+                /* Optimize: check if the element is too large or the list
+                 * becomes too long *before* executing zzlInsert. */
+                zobj->ptr = zzlInsert(zobj->ptr,ele,score);
+                if (zzlLength(zobj->ptr) > server.zset_max_ziplist_entries)
+                    zsetConvert(zobj,REDIS_ENCODING_SKIPLIST);
+                if (sdslen(ele->ptr) > server.zset_max_ziplist_value)
+                    zsetConvert(zobj,REDIS_ENCODING_SKIPLIST);
+
+                signalModifiedKey(c->db,key);
+                server.dirty++;
+                if (!incr) added++;
+            }
+        } else if (zobj->encoding == REDIS_ENCODING_SKIPLIST) {
+            zset *zs = zobj->ptr;
+            zskiplistNode *znode;
+            dictEntry *de;
+
+            ele = c->argv[3+j*2] = tryObjectEncoding(c->argv[3+j*2]);
+            de = dictFind(zs->dict,ele);
+            if (de != NULL) {
+                curobj = dictGetKey(de);
+                curscore = *(double*)dictGetVal(de);
+
+                if (incr) {
+                    score += curscore;
+                    if (isnan(score)) {
+                        addReplyError(c,nanerr);
+                        /* Don't need to check if the sorted set is empty
+                         * because we know it has at least one element. */
+                        zfree(scores);
+                        return;
+                    }
+                }
+
+                /* Remove and re-insert when score changed. We can safely
+                 * delete the key object from the skiplist, since the
+                 * dictionary still has a reference to it. */
+                if (score != curscore) {
+                    redisAssertWithInfo(c,curobj,zslDelete(zs->zsl,curscore,curobj));
+                    znode = zslInsert(zs->zsl,score,curobj);
+                    incrRefCount(curobj); /* Re-inserted in skiplist. */
+                    dictGetVal(de) = &znode->score; /* Update score ptr. */
+
+                    signalModifiedKey(c->db,key);
+                    server.dirty++;
+                }
+            } else {
+                znode = zslInsert(zs->zsl,score,ele);
+                incrRefCount(ele); /* Inserted in skiplist. */
+                redisAssertWithInfo(c,NULL,dictAdd(zs->dict,ele,&znode->score) == DICT_OK);
+                incrRefCount(ele); /* Added to dictionary. */
+
+                signalModifiedKey(c->db,key);
+                server.dirty++;
+                if (!incr) added++;
+            }
+        } else {
+            redisPanic("Unknown sorted set encoding");
+        }
+    }
+    zfree(scores);
+    if (incr) /* ZINCRBY */
+        addReplyDouble(c,score);
+    else /* ZADD */
+        addReplyLongLong(c,added);
+}
+
+void iaddCommand(redisClient *c) {
+    iaddGenericCommand(c,0);
+}
