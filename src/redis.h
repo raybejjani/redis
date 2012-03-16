c3164ddbe5497dc4a0bd6e757fb783b432df4f75
bill <bill.mill@gmail.com>
2012-03-16 15:25:21 -0400

    adding REDIS_ENCODING_AVLTREE constant to redis.h

--- src/redis.h
+++ src/redis.h
@@ -103,6 +103,7 @@
 #define REDIS_ENCODING_ZIPLIST 5 /* Encoded as ziplist */
 #define REDIS_ENCODING_INTSET 6  /* Encoded as intset */
 #define REDIS_ENCODING_SKIPLIST 7  /* Encoded as skiplist */
+#define REDIS_ENCODING_AVLTREE 8   /* Encoded as AVL Tree */
 
 /* Defines related to the dump file format. To store 32 bits lengths for short
  * keys requires a lot of space, so we check the most significant 2 bits of
