16923e19fbd21ee3585678b4511f4f9f3666e06a
bill <bill.mill@gmail.com>
2012-03-23 22:28:20 -0400

    adding a boundary test

--- tests/unit/type/iset.tcl
+++ tests/unit/type/iset.tcl
@@ -5,5 +5,13 @@ start_server {tags {"iset"}} {
         r iadd itmp 12 22 y
         r iadd itmp 15 25 z
         assert_equal_elements {x y z} [r istab itmp 18]
+        assert_equal_elements {y z} [r istab itmp 21]
+    }
+
+    test "ISET Boundaries" {
+        r del itmp
+        r iadd itmp 10 20 x
+        assert_equal {x} [r istab itmp 10]
+        assert_equal {x} [r istab itmp 20]
     }
 }
